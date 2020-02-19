018bd550b9de658829f22fab4add56674cd85d4f
mrfarukturgut <farukturgutiu@gmail.com>
2020-02-19 20:01:34 +0300

    Initial Commit

---
+++ FMSS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FMSS
+//
+//  Created by Faruk Turgut on 19.02.2020.
+//  Copyright © 2020 Faruk Turgut. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
